64d5eb46b6a79e6dbce19d05aba5f771df9fefb5
piñas <dego2002@hotmail.com>
2026-01-29 11:43:22 -0600

    prueba de git funcionando

---
+++ Python/funciones/funcion_ejem1.py
@@ -0,0 +1,7 @@
+# 1. Definimos la función
+def saludar_invitado(nombre):
+    print(f"Hola {nombre}, bienvenido a la base de datos.")
+
+# 2. La usamos (esto se llama "llamar" a la función)
+saludar_invitado("Maite")
+saludar_invitado("Diego")
